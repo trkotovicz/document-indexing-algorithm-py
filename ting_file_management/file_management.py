7d240daa993f17ddf8e59fe363cb0edb4459582c
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-12-08 00:27:12 -0300

    feat: implementa o método process no módulo file_process

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,4 +1,3 @@
-# from sys import stderr
 import sys
 
 
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file, instance: Queue):
+    if path_file in str(instance._data):
+        return ''
+
+    text = txt_importer(path_file)
+
+    result_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(text),
+        "linhas_do_arquivo": text
+    }
+
+    instance.enqueue(result_dict)
+    print(result_dict)
 
 
 def remove(instance):
