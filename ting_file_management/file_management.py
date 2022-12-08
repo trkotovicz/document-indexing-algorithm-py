f1c7b6771c3166c183f870752c27ec90ddfde1cb
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-12-07 21:47:05 -0300

    feat: implementa a função txt_importer dentro do módulo file_management

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+# from sys import stderr
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        sys.stderr.write('Formato inválido\n')
+
+    try:
+        with open(path_file, 'r') as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+# https://www.delftstack.com/pt/howto/python/python-print-to-stderr/#utilize-o-sysstderr-para-imprimir-para-o-stderr
